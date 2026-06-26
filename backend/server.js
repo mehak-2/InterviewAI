b26e8b290774be01d757c0e9bf32e46260bdf70b
mehakmehak221 <mehak.gaur@maxtron.ai>
2026-06-27 01:09:58 +0530

    Configure server.js to export app and bypass listen in serverless environments

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ import connectDB from "./src/config/db.js";
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (!process.env.VERCEL) {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(
+            `Server running on port ${PORT}`
+        );
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(
-        `Server running on port ${PORT}`
-    );
-});
+export default app;
